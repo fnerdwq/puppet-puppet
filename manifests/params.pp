51becf12d57fc5db8f44ab34d4d35f48e2e3cc4c
Frederik Wagner <wagner@wagit.de>
2014-05-15 10:19:41 +0200

    introduced complete params class handling

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,34 @@
 # set flavor specific variables (private)
 class puppet::params {
-
+  # Variables
   $config_dir = '/etc/puppet'
 
+  # Module Parameter
+  $facter               = 'latest'
+  $hiera                = 'latest'
+  $version              = 'latest'
+  $agent_name           = 'puppet'
+  $agent_enable         = true
+  $agent_config         = { 'pluginsync' => 'true',
+                            'server'     => "puppet.${::domain}" }
+  $master               = false
+  $master_name          = 'puppetmaster'
+  $master_enable        = true
+  $master_config        = {}
+  $passenger            = true
+  $inventory            = false
+  $inventory_allow      = "dashboard.${::domain}"
+  $environments_config  = {}
+  $environments_dir     = undef
+  $environments_owner   = 'puppet'
+  $environments_group   = 'puppet'
+  $hiera_datadir        = "${config_dir}/hieradata"
+  $hiera_datadir_create = true
+  $hiera_hierarchy      = ['%{::clientcert}', 'common']
+  $autosign_list        = []
+  $fileserver           = {}
+
+  # Further Variables
   case $::osfamily {
     'Debian': {
       $apache_service_name = 'apache2'
